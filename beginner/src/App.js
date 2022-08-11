611620f2b02d49ebdec72d9592a4b643856f35a4
PARK-JONGSEOK <pjongsuk1@gmail.com>
2022-08-11 10:11:01 +0900

    useEffect

--- beginner/src/App.js
+++ beginner/src/App.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prevState) => prevState + 1);
-  console.log("Call an API");
+  console.log("I run all the time.");
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
